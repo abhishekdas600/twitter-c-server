2a0d7437d41e3600766fd660a8be49bbac8fcfed
abhishekdas <dasabhshek522@gmail.com>
2023-11-14 13:02:21 +0530

    complete authentication

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,19 +1,23 @@
 import express from "express";
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
+import cors from 'cors';
 import bodyParser from "body-parser";
 import { PrismaClient } from "@prisma/client";
 import { prismaClient } from "../clients/db";
 import { User } from "./user";
+import { GraphqlContext } from "../interfaces";
+import JWTService from "../services/jwt";
 
 export async function initServer() {
     const app = express();
 
    
 
-    app.use(bodyParser.json())
+    app.use(bodyParser.json());
+    app.use(cors());
 
-    const graphqlServer = new ApolloServer({
+    const graphqlServer = new ApolloServer<GraphqlContext>({
         typeDefs : `
 
         ${User.types}
@@ -30,7 +34,13 @@ export async function initServer() {
 
       await graphqlServer.start();
 
-    app.use("/graphql", expressMiddleware(graphqlServer));
+    app.use("/graphql", expressMiddleware(graphqlServer, {context: async({req,res})=>{
+        return{
+            user:req.headers.authorization 
+            ? JWTService.decodeToken(req.headers.authorization.split('Bearer ')[1]) 
+            : undefined,
+        }
+    }}));
 
     return app;
 }
